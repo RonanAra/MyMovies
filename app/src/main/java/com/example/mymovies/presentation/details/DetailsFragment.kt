74c57ef5e515e58410993c4b5beeb1af6e670a06
Ronan Fernandes <ronanlm10@hotmail.com>
2023-02-16 19:45:41 -0300

    Feat: Create Fragments

---
+++ app/src/main/java/com/example/mymovies/presentation/details/DetailsFragment.kt
@@ -0,0 +1,38 @@
+package com.example.mymovies.presentation.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mymovies.databinding.FragmentDetailsBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentDetailsBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(
+            view,
+            savedInstanceState
+        )
+    }
+}
